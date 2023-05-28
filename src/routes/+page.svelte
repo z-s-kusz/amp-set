6da331f54f626daa89559a390222d86018a5db19
Zach Kusz <zack.kusz@capitollien.com>
2023-05-28 13:05:38 -0500

    fix header style

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -2,7 +2,7 @@
     const chargeList = ['1s', '2s', '3s', '4s', '5s', '6s'];
 </script>
 
-<header class="hero hero-content">
+<header class="card text-center">
     <h1 class="text-2xl">Select a cell count to get charging!</h1>
     <p class="text-sm">* Not Liable for any damage caused by charging</p>
 </header>
